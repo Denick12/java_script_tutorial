ef64df420ec26979d9b0fda7b616641c2d0a67d3
Denick <catlynsmith036@gmail.com>
2025-08-02 13:37:58 +0300

    variables

--- templates/index.js
+++ templates/index.js
@@ -5,6 +5,19 @@
 //A run time env used to execute javascript code is called node - node index.js
 
 // using let to declare variable instead of var
+// when creating variables;
+// 1.you cannot use reserved keywords
+// 2.should be meaningful
+// 3.Cannot start with a number (ie; a variable cannot 1name)
+// 4. Are case-sensitive
+
+
 let name = 'Mash';
 console.log(name);
+
+let firstName = 'Mosh';
+let lastName = 'Karan';
+console.log(firstName,lastName)
+
+
 
