da3063b0deac0d0be5a12c6783ba586f0fbaf500
Denick <catlynsmith036@gmail.com>
2025-08-02 13:28:16 +0300

    creating a variable in js using let

---
+++ templates/index.js
@@ -0,0 +1,10 @@
+// This is my first Javascript code!
+// console.log('Hello World');
+
+
+//A run time env used to execute javascript code is called node - node index.js
+
+// using let to declare variable instead of var
+let name = 'Mash';
+console.log(name);
+
