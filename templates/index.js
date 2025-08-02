8d50b04f4fb2ee4f2273a6f7d2fcafbf065bdb72
Denick <catlynsmith036@gmail.com>
2025-08-02 14:23:15 +0300

    creating objects in js

--- templates/index.js
+++ templates/index.js
@@ -32,12 +32,28 @@ console.log(principalRate)
 
 // in java, we have 2 categories of data types; primitive/value types, reference type
 // primitive/value types : strings, booleans, Number, undefined, null
-
+// Reference types include Object,  Array, Function
+// An object in programming is like an object in real life think of a person who has attributes age, name, address {
+// all these are properties of a person}
+// when dealing with multiple related variables, they can be enclosed in an object
 let isApproved = true  //Boolean
 let myfirstName = undefined  //undefined
 let age = 30 //number
 let myLastName = null //null
 let selectedColor = null
+
+// java is a dynamic language
+
+// creating an object
+let myname = 'Denick'
+let myAge = 50
+// Now I proceed to create an object person
+let person = {
+    name: 'DG',
+    age : '20'
+};
+console.log(person)
+
 
 
 
