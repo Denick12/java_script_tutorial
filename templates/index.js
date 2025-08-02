f2fe0d681e8f724f0fce0fd6f4d8ff9a7a8bcb08
Denick <catlynsmith036@gmail.com>
2025-08-02 15:56:58 +0300

    calculating function

--- templates/index.js
+++ templates/index.js
@@ -128,6 +128,22 @@ let number = square(2)
 console.log(number)
 
 //in console.log function, you can call in another function as above--- console.log(square(2)) or a simple string as console.log('Hello')
+let baseSalary = 30000;
+let overtime =10;
+let rate = 20;
+
+function getWage(baseSalary, overtime, rate){
+    return baseSalary + (overtime * rate);
+}
+let employee = {
+    baseSalary: 30000,
+    overtime:10,
+    rate:20,
+    getWage:function (){
+     return this.baseSalary + (this.overtime*this.rate);
+    }
+};
+employee.getWage();
 
 
 
