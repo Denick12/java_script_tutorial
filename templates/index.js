248444e0289074d5616446cab4fd7fe6ccc48d8e
Denick <catlynsmith036@gmail.com>
2025-08-04 20:32:37 +0300

    js

--- templates/index.js
+++ templates/index.js
@@ -132,18 +132,7 @@ let baseSalary = 30000;
 let overtime =10;
 let rate = 20;
 
-function getWage(baseSalary, overtime, rate){
-    return baseSalary + (overtime * rate);
-}
-let employee = {
-    baseSalary: 30000,
-    overtime:10,
-    rate:20,
-    getWage:function (){
-     return this.baseSalary + (this.overtime*this.rate);
-    }
-};
-employee.getWage();
+
 
 
 
