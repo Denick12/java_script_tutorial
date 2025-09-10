916b4f5952121bb139e22517e2c1e779ad15a1a0
Denick <catlynsmith036@gmail.com>
2025-09-10 05:52:57 +0300

    adding values on our todo list array

--- templates/scripts/11-todo-list.js
+++ templates/scripts/11-todo-list.js
@@ -4,5 +4,8 @@ const todoList = [];
 function addTodo(){
     const inputElement = document.querySelector(".js-name-input");
     const name= inputElement.value;
-    console.log(name);
+    // console.log(name);
+
+    todoList.push(name);
+    console.log(todoList)
 }
