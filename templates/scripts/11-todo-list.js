1bc9a91db457d94c165ff77e753f86f41e742606
Denick <catlynsmith036@gmail.com>
2025-09-16 09:11:06 +0300

    todo list html

--- templates/scripts/11-todo-list.js
+++ templates/scripts/11-todo-list.js
@@ -5,7 +5,11 @@
 // console.log the array
 
 const todoList = [];
-
+function addTodo (){
+    const inputElement= document.querySelector('.js-name-input');
+    const name = inputElement.value
+    console.log(name)
+}
 
 
 
